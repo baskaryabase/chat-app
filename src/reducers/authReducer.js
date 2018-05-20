c4ce1bf843cfdad14a7269d036d7e807e326fd11
Unknown <baymax1298@gmail.com>
2018-05-20 16:57:04 +0530

    reducers commited

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,44 @@
+"use strict";
+
+export function authReducer(state = { user: { loggedin: null } }, action) {
+  switch (action.type) {
+    case "signUp":
+      localStorage.setItem("token", action.payload.token);
+      return {
+        user: action.payload.user,
+        loggedin: true
+      };
+      break;
+
+    case "signIn":
+      localStorage.setItem("token", action.payload.token);
+      return {
+        user: action.payload.user,
+        loggedin: true
+      };
+      break;
+
+    case "logout":
+      localStorage.removeItem("token");
+      return {
+        user: {},
+        loggedin: false
+      };
+      break;
+
+    case "getAuth":
+      return {
+        user: action.payload,
+        loggedin: true
+      };
+      break;
+    case "getAuthRejected":
+      return {
+        user: action.payload,
+        loggedin: false
+      };
+      break;
+  }
+
+  return state;
+}
